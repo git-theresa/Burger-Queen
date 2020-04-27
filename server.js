df35034d2975d15b981a89a9b0d6445862bc22ca
Theresa E <t.eatherly@gmail.com>
2020-04-27 09:23:42 -0500

    all files loaded

--- server.js
+++ server.js
@@ -1,104 +1,26 @@
-var mysql = require("mysql");
 var express = require("express");
-var exphbs = require("express-handlebars");
 
-// Create instance of express app.
-var app = express();
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-// MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "seinfeld"
-});
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Initiate MySQL Connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-// Data
-var lunches = [
-  {
-    lunch: "Beet & Goat Cheese Salad with minestrone soup."
-  }, {
-    lunch: "Pizza, two double veggie burgers, fries with a Big Gulp"
-  }
-];
-// Routes
-app.get("/cast", function(req, res) {
-  connection.query("SELECT * FROM actors ORDER BY id", function(err, result) {
-    if (err) throw err;
-    
-    var html = "<h1>Actors Ordered BY ID</h1>";
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
 
-    html += "<ul>";
-
-    for (var i = 0; i < result.length; i++) {
-      html += "<li><p> ID: " + result[i].id + "</p>";
-      html += "<p> Name: " + result[i].name + "</p>";
-      html += "<p> Coolness Points: " + result[i].coolness_points + "</p>";
-      html += "<p>Attitude: " + result[i].attitude + "</p></li>";
-    }
-
-    html += "</ul>";
-
-    res.send(html);
-  });
-});
-
-app.get("/coolness-chart", function(req, res) {
-  connection.query("SELECT * FROM actors ORDER BY coolness_points DESC", function(err, result) {
-    if (err) throw err;
-
-    var html = "<h1>Actors by Coolness</h1>";
-
-    html += "<ul>";
-
-    for (var i = 0; i < result.length; i++) {
-      html += "<li><p> ID: " + result[i].id + "</p>";
-      html += "<p> Name: " + result[i].name + "</p>";
-      html += "<p> Coolness Points: " + result[i].coolness_points + "</p>";
-      html += "<p>Attitude: " + result[i].attitude + "</p></li>";
-    }
-
-    html += "</ul>";
-
-    res.send(html);
-  });
-});
-
-app.get("/attitude-chart/:att", function(req, res) {
-  connection.query("SELECT * FROM actors WHERE attitude = ?", [req.params.att], function(err, result) {
-    if (err) throw err;
-
-    var html = "<h1>Actors With an Attitude of " + req.params.att + "</h1>";
-
-    html += "<ul>";
-
-    for (var i = 0; i < result.length; i++) {
-      html += "<li><p> ID: " + result[i].id + "</p>";
-      html += "<p> Name: " + result[i].name + "</p>";
-      html += "<p> Coolness Points: " + result[i].coolness_points + "</p>";
-      html += "<p>Attitude: " + result[i].attitude + "</p></li>";
-    }
-
-    html += "</ul>";
-
-    res.send(html);
-  });
-});
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
