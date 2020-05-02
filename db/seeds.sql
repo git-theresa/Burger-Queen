53ffdfc46af08d03d128b7e428c24ae0be23f382
Theresa E <t.eatherly@gmail.com>
2020-05-01 19:01:00 -0500

    seeds updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,18 +8,13 @@
 
 INSERT INTO burgers (name) VALUES ('The Fuh-gouda-bout-it Burger');
 INSERT INTO burgers (name) VALUES ('Little Chop of Horseradish Burger');
-INSERT INTO burgers (name, devoured ) VALUES ('My Bloody Kale-entine Burger', true);
+INSERT INTO burgers (name, devoured ) VALUES ('Smells Like Bean Spirit', true);
 INSERT INTO burgers (name, devoured) VALUES ('The Absentee Shallot Burger', true);
 INSERT INTO burgers (name, devoured) VALUES ('The Land of the Slaw-st Burger', true);
 INSERT INTO burgers (name) VALUES ('Focaccia Red Handed Burger');
-INSERT INTO burgers (name) VALUES ('Tank God Its Fried Egg Burger');
-INSERT INTO burgers (name) VALUES ('She is a Super Leek Burger');
+INSERT INTO burgers (name) VALUES ('Thank God Its Fried Egg Burger');
+INSERT INTO burgers (name) VALUES ('She a Super Leek Burger');
 INSERT INTO burgers (name, devoured ) VALUES ('The Dont Dream Its Okra Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('The Stop or My Mom Will Shoots Burger', true);
 INSERT INTO burgers (name, devoured) VALUES ('Two Karat Burger', true);
-INSERT INTO burgers (name) VALUES ('Say it Aint Cilantro Burger');
-INSERT INTO burgers (name) VALUES ('Not If I Can Kelp It Burger');
-INSERT INTO burgers (name) VALUES ('The Jack-O-Lentil Burger');
-INSERT INTO burgers (name, devoured ) VALUES ('The Whats This Lime For? Burger', true);
 INSERT INTO burgers (name, devoured) VALUES ('The Cops and Rabe-ers Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Take Me Out To The Burger', true);
+
